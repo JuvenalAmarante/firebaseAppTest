7085e35efdce1eda4ffa309d9541ae573162e992
Juvenal Amarante <juvenalamarante13@gmail.com>
2019-01-09 16:53:12 -0300

    (paginas) - Adicionando pagina Home com SideMenu

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, DrawerLayoutAndroid, } from 'react-native'
 
+import Drawer from './../../config/Drawer'
 export default class HomePage extends Component {
 
     render() {
         return (
-            <View>
+            <DrawerLayoutAndroid
+              drawerWidth={styles.drawerWidth}
+              drawerPosition={DrawerLayoutAndroid.positions.Left}
+              renderNavigationView={Drawer}>
                 <Text>Bem vindo a HomePage</Text>
-            </View>
+            </DrawerLayoutAndroid>
         );
     }
 }
